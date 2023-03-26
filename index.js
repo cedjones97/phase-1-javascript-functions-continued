6af75c5eb3f04e634856d0af2f32bf13bd3ec400
cedric <cedricj765@gmail.com>
2023-03-26 17:51:42 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+// razzle("Alicia")
+
+// function razzle(laywer = "Billy", target = "'em"){
+
+//     console.log(`${laywer} razzle-dazzles ${target}!`)
+// }
+// razzle()
+// razzle("Cedric", "them")
+// razzle("Amariah")
+// razzle("Billy", "them")
+
+function saturdayFun(activity = "roller-skate"){
+
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork(workActivity = "go to the office"){
+
+    return `This Monday, I will ${workActivity}.`
+}
+
+function wrapAdjective(wrap = "*"){
+
+    return function (adjective = "special"){
+
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+
+    
+} 
+
+
+
+
+
+
